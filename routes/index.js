607e2d616869878befa919ca797c788348d5aa5e
Mahmoud Khaled <mahmoud_khld@yahoo.com>
2018-07-28 17:30:55 +0200

    add contact page

--- routes/index.js
+++ routes/index.js
@@ -7,4 +7,12 @@ router.get('/', function(req, res, next) {
   res.render('landpage');
 });
 
+router.get('/about', (req, res) => {
+  res.render('about');
+});
+
+router.get('/contact', (req, res) => {
+  res.render('contact');
+});
+
 module.exports = router;
